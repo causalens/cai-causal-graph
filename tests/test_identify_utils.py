f3c25e51647013c6fc6faf22b515ab10db5c1cb6
Steven Kleinegesse <steven@causalens.com>
2023-10-02 12:45:00 +0100

    andrew comments

--- tests/test_identify_utils.py
+++ tests/test_identify_utils.py
@@ -1,10 +1,23 @@
 """
-Copyright (c) 2023 by Impulse Innovations Ltd. Private and confidential. Part of the causaLens product.
+Copyright 2023 Impulse Innovations Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 import unittest
 
 from cai_causal_graph import CausalGraph
 from cai_causal_graph.identify_utils import identify_confounders
+from cai_causal_graph.type_definitions import EDGE_T
 
 
 class TestIdentifyConfounders(unittest.TestCase):
@@ -63,3 +76,14 @@ class TestIdentifyConfounders(unittest.TestCase):
         # compute confounders between treatment and outcome
         confounders = identify_confounders(self.graph_4, source='x', destination='y')
         self.assertSetEqual(set(confounders), {'u', 'z'})
+
+    def test_non_dag(self):
+        # create a mixed graph
+        graph = CausalGraph()
+        graph.add_edge('z', 'u', edge_type=EDGE_T.UNDIRECTED_EDGE)
+        graph.add_edge('z', 'y')
+        graph.add_edge('u', 'x')
+        graph.add_edge('x', 'y')
+
+        with self.assertRaises(TypeError):
+            identify_confounders(graph, source='x', destination='y')
