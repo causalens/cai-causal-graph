82de38e4c62eb05317f6def3d8cc46f5e32d8be6
Steven Kleinegesse <steven@causalens.com>
2023-10-03 14:55:14 +0100

    finished identify_instruments

--- cai_causal_graph/identify_utils.py
+++ cai_causal_graph/identify_utils.py
@@ -83,3 +83,65 @@ def identify_confounders(graph: CausalGraph, node_1: str, node_2: str) -> List[s
     minimal_confounders = confounders.intersection(confounders_reverse)
 
     return list(minimal_confounders)
+
+
+def identify_instruments(graph: CausalGraph, source: str, destination: str) -> List[str]:
+    """
+    Identify all instrumental variables for the causal effect of `source` and `destination` in the provided `graph`.
+
+    An instrumental variable for the causal effect of `source` on `destination` satisfies the following criteria:
+        1. There is a causal effect between the `instrument` and the `source`.
+        2. The `instrument` has a causal effect on the `destination` _only_ through the `source`.
+        3. There is no confounding between the `instrument` and the `destination`.
+
+    Example:
+        >>> from typing import List
+        >>> from cai_causal_graph import CausalGraph
+        >>> from cai_causal_graph.identify_utils import identify_instruments
+        >>>
+        >>> # define a causal graph
+        >>> cg = CausalGraph()
+        >>> cg.add_edge('z', 'x')
+        >>> cg.add_edge('u', 'x')
+        >>> cg.add_edge('u', 'y')
+        >>> cg.add_edge('x', 'y')
+        >>>
+        >>> # find the instruments between 'x' and 'y'; output: ['z']
+        >>> instrumental_variables: List[str] = identify_instruments(cg, source='x', destination='y')
+
+    :param graph: The causal graph given by a `cai_causal_graph.causal_graph.CausalGraph` instance. This must be a DAG,
+        i.e. it must only contain directed edges and be acyclic, otherwise a `TypeError` is raised.
+    :param source: The source variable.
+    :param destination: The destination variable.
+    :return: A list of instrumental variables for the causal effect of `source` on `destination`.
+    """
+    if not graph.is_dag():
+        raise TypeError(f'Expected a DAG, but got a mixed causal graph.')
+
+    # get all the confounders between the source and destination
+    confounders = identify_confounders(graph, source, destination)
+
+    # get all the ancestors of the source that are not in the above confounding set
+    candidate_nodes = set(graph.get_ancestors(source)).difference(confounders)
+
+    # reject all candidate nodes that have a directed path to any confounders, and vice versa
+    for confounder in confounders:
+        confounder_descendants = graph.get_descendants(confounder)
+        confounder_ancestors = graph.get_ancestors(confounder)
+        for candidate in candidate_nodes.copy():
+            if candidate in confounder_descendants or candidate in confounder_ancestors:
+                candidate_nodes.remove(candidate)
+
+    # reject all candidate nodes that have a directed path to the destination that does not go through the source
+    for candidate in candidate_nodes.copy():
+        for causal_path in graph.get_all_causal_paths(candidate, destination):
+            if source not in causal_path:
+                candidate_nodes.remove(candidate)
+                break
+
+    # reject all candidate nodes that have a non-empty confounding set between itself and the destination
+    for candidate in candidate_nodes.copy():
+        if len(identify_confounders(graph, candidate, destination)) > 0:
+            candidate_nodes.remove(candidate)
+
+    return list(candidate_nodes)
