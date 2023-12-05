38fb01d8a0c23c9d8303777567a28ae7956e5cd6
Patricia Jacob <104359539+patricia-causalens@users.noreply.github.com>
2023-12-05 10:31:58 +0000

    Fix: Skeleton missing __iter__ (#36)
    Description and Motivation
    When adding support for Skeleton on Dara, the graph was not json serialised correctly by fastapi. The reason being that __iter__ was missing from the class.
    
    Additional Context
    Needed for Dara to support Skeleton graphs on CausalGraphViewer component.
    
    How Has This Been Tested?
    Tested that this fixed the issue I was having on Dara by manually applying this changes within the env

--- cai_causal_graph/causal_graph.py
+++ cai_causal_graph/causal_graph.py
@@ -94,6 +94,11 @@ class Skeleton(CanDictSerialize, CanDictDeserialize):
         """Check if the skeleton is not equal to another skeleton."""
         return not (self == other)
 
+    def __iter__(self) -> Iterator:
+        """Return item,value tuples of the content of `self.to_dict()`."""
+        for k, v in self.to_dict().items():
+            yield k, v
+
     @property
     def nodes(self) -> List[Node]:
         """Return a list of all nodes."""
@@ -421,7 +426,7 @@ class CausalGraph(HasIdentifier, HasMetadata, CanDictSerialize, CanDictDeseriali
             return self.get_edge(item[0], item[1])
 
     def __iter__(self) -> Iterator:
-        """Return item,value tuples of the content of self.to_dict()."""
+        """Return item,value tuples of the content of `self.to_dict()`."""
         for k, v in self.to_dict().items():
             yield k, v
 
